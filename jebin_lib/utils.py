6d7055f3ae985d672dbd973d954ded80d9ca061b
jebin2 <jebineinstein@gmail.com>
2026-03-04 19:21:52 +0530

    is_valid_json

--- jebin_lib/utils.py
+++ jebin_lib/utils.py
@@ -17,3 +17,34 @@ def is_valid_audio(file_path):
         return result.returncode == 0 and result.stdout != b""
     except Exception:
         return False
+
+def is_valid_video(file_path):
+    if not os.path.exists(file_path):
+        return False
+
+    if os.path.getsize(file_path) < 100:
+        return False
+
+    try:
+        result = subprocess.run(
+            ["ffprobe", "-v", "error", "-show_streams", "-select_streams", "v", file_path],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        return result.returncode == 0 and result.stdout != b""
+    except Exception:
+        return False
+
+def is_valid_json(file_path):
+    if not os.path.exists(file_path):
+        return False
+
+    if os.path.getsize(file_path) < 100:
+        return False
+
+    try:
+        with open(file_path, 'r') as f:
+            json.load(f)
+        return True
+    except Exception:
+        return False
